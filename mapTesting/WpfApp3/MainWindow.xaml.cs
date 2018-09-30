68b3dd6615893e1fee8b58af8d4cd493acfce0b9
MariaHokas <42113018+MariaHokas@users.noreply.github.com>
2018-09-30 14:40:10 +0300

    Concole application ready

---
+++ mapTesting/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttom_Click(object sender, RoutedEventArgs e)
+        {
+            string street = txt_street.Text;
+            string city = txt_city.Text;
+            string stage = txt_stage.Text;
+            string zip = txt_zip.Text;
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("http://maps.google.com/maps?q=");
+                if (street != string.Empty)
+                {
+                    queryaddress.Append(street + "," + "+");
+                }
+                if (city != string.Empty)
+                {
+                    queryaddress.Append(city + "," + "+");
+                }
+                if (stage != string.Empty)
+                {
+                    queryaddress.Append(stage + "," + "+");
+                }
+                if (zip != string.Empty)
+                {
+                    queryaddress.Append(zip + "," + "+");
+                }
+                webBrowser1.Navigate(queryaddress.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+    }
+}
